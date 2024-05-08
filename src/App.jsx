80d603b8da02bf62232873861031e5b4585ff5cd
vikry28 <vikryt@gmail.com>
2024-05-08 23:32:47 +0700

    develop_add_todo_feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
-import React, { useState } from 'react'
+import React, { useState, createContext } from 'react'
 import Todos from './components/Todos';
+import TodoForm from './components/TodoForm';
+
+export const TodoContext = createContext()
 
 function App() {
   const [todos, setTodos] = useState([
@@ -18,27 +21,67 @@ function App() {
       title: 'Study React with Ninja Ken',
       completed: false,
     },
-  ])
+  ]);
 
-  console.log(todos)
+  const toggleCompleted = (todoId) => {
+    const updatedTodos = todos.map((todo) => {
+      if (todo.id === todoId) {
+        todo.completed = !todo.completed;
+      }
+      return todo;
+    });
+    setTodos(updatedTodos);
+  };
+
+  const addTodo = (todoTitle) => {
+    if (todoTitle === '') {
+      return;
+    }
+
+    const newTodo = {
+      id: todos.length + 1,
+      title: todoTitle,
+      completed: false,
+    };
+
+    const updatedTodos = todos.concat(newTodo);
+    setTodos(updatedTodos);
+  };
+
+  const deleteTodo = (todoId) => {
+    const updatedTodos = todos.filter((todo) => todo.id !== todoId);
+    setTodos(updatedTodos);
+  };
 
   return (
-    <div style={styles.container}>
-      <h1 style={styles.title}>My Todo List</h1>
-      <Todos todos={todos} />
-    </div>
+    // Bungkus app dengan provider dari context
+    <TodoContext.Provider value={{ toggleCompleted, deleteTodo }}>
+      <div style={styles.container}>
+        <h1 style={styles.title}>My Todo List</h1>
+        <TodoForm addTodo={addTodo} />
+        <Todos todos={todos} />
+      </div>
+    </TodoContext.Provider>
   )
 }
 
+
 const styles = {
   container: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    minHeight: '100vh',
+    padding: '20px',
     textAlign: 'center',
-    padding: '12px',
+    margin: 'auto', // Menempatkan konten di tengah layar
+    maxWidth: '600px', // Membatasi lebar konten agar tetap responsif
   },
   title: {
     fontSize: '36px',
+    marginBottom: '20px',
   },
-}
-
+};
 
 export default App
