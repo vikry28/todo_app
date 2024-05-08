e4dd8c30fb6ee9706ab466d3930166a8f791b7ba
vikry28 <vikryt@gmail.com>
2024-05-08 23:49:35 +0700

    penambahan useContext

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,18 +1,55 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { TodoContext } from '../App'
 
 const TodoItem = ({ todo }) => {
-    return (
-        <div style={styles.todoItem}>
-            <p>{todo.title}</p>
-        </div>
-    )
-}
+  const { toggleCompleted, deleteTodo } = useContext(TodoContext)
+  const getTodoTitleStyle = () => {
+    if (todo.completed === true) {
+      return { textDecoration: 'line-through' };
+    } else {
+      return { textDecoration: 'none' };
+    }
+  };
+  
+
+  return (
+    <div style={styles.todoItem}>
+      <input
+        type="checkbox"
+        style={styles.checkbox}
+        onChange={() => toggleCompleted(todo.id)}
+      />
+      <p style={{ ...styles.todoTitle, ...getTodoTitleStyle() }}>{todo.title}</p>
+      <button style={styles.button} onClick={() => deleteTodo(todo.id)}>
+        x
+      </button>
+    </div>
+  );
+};
 
 const styles = {
-    todoItem: {
-        border: '2px solid #f4f4f4',
-        fontSize: '24px',
-    },
+  todoItem: {
+    border: '2px solid #f4f4f4',
+    fontSize: '24px',
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: '0 20px',
+  },
+  checkbox: {
+    height: '18px',
+    width: '18px',
+  },
+  button: {
+    backgroundColor: '#BB0000',
+    color: '#fff',
+    height: '30px',
+    width: '30px',
+    borderRadius: '100%',
+    border: 'none',
+    cursor: 'pointer',
+    fontSize: '16px',
+  },
 }
 
 export default TodoItem
--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,21 +1,27 @@
-import React from 'react'
+import React from 'react';
 import TodoItem from './TodoItem'
 
-const Todos = ({ todos }) => {
+
+const Todos = ({ todos, toggleCompleted }) => {
   return (
     <div style={styles.container}>
       {todos.map((todo) => {
-        return <TodoItem key={todo.id} todo={todo} />
+        return (
+          <TodoItem
+            key={todo.id}
+            todo={todo}
+            toggleCompleted={toggleCompleted}
+          />
+        );
       })}
     </div>
-  )
-}
+  );
+};
 
 const styles = {
   container: {
-    width: '40%',
-    margin: '0 auto',
+    width: '100%', // Kontainer mengisi lebar maksimum yang tersedia
   },
-}
+};
 
 export default Todos
